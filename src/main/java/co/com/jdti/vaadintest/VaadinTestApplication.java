570356067e6b85ccfc7c41f563a4e4a63884fc3b
Yisus <jesu_ti91@hotmail.com>
2024-12-07 10:10:09 -0500

    Vaadin

--- src/main/java/co/com/jdti/vaadintest/VaadinTestApplication.java
+++ src/main/java/co/com/jdti/vaadintest/VaadinTestApplication.java
@@ -1,8 +1,11 @@
 package co.com.jdti.vaadintest;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Theme(value = "my-app")
 @SpringBootApplication
 public class VaadinTestApplication implements AppShellConfigurator {
 
